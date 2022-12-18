561cb77a73176413a42e801d48c6524c68748b96
Abram Kremer <a.kremer@columbia.edu>
2022-12-18 14:40:23 -0500

    Updated writeup

--- austin_smart_all_x_bot.py
+++ austin_smart_all_x_bot.py
@@ -18,7 +18,7 @@ end of that round.
 
 This smart version of the bot is the same as the original All X's Bot except with
 the new addition of logic surround the use of the H gate in the end game.
-The thought process behind this new logic was surround the low probability that a
+The thought process behind this new logic was surrounding the low probability that a
 bot is able to get a hand full of X gates before round 100.
 This probability was calculated to be only 18.21%, so to accomodate for this low
 probability, after round 70, our bot also allows H gates to take a position in our hand.
@@ -27,8 +27,8 @@ much higher probability we calculate to be 92.98%
 These H gates often do not provide us with as large an advantage as X gates do, 
 but in many cases an H gate can still pull the state closer to our team.
 
-Probabilities were found using binomial probability calculations, given that we know 
-the probabilities of each card being distributed to our team on a given round.
+Probabilities were found using binomial probability calculations, using the known 
+probabilities of each card being distributed to our team on a given round.
 '''
 class MyStrategy(GameBot):
     '''
